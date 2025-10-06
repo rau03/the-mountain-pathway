0e4743995d1d0efa6b04b5aeb4ba2171a22fb2e1
Chris Rau <98765332+rau03@users.noreply.github.com>
2025-10-05 20:29:18 -0500

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/mountain-icon.svg" type="image/svg+xml" />
         <link
           rel="preload"
           href="/homepage-background.v3.jpg"
